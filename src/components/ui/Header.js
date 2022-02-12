a6e55e5f007b791f46d1c6d8373da8ca20738d12
Richard Dobeck <rdobeck@yahoo.com>
2022-02-12 14:43:47 -0800

    created basic Header, importing into App

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { AppBar, Toolbar } from "@mui/material";
+
+
+export default function Header() {
+    return (
+        <AppBar position="fixed">
+            <Toolbar>
+                Arc Development
+            </Toolbar>
+        </AppBar>
+    )
+}
